9875c2925712fff88f934cca787387ab32778ae9
Zahragrs <dryagaroosi@gmail.com>
2025-07-22 15:56:33 +0330

    add

--- courses/models.py
+++ courses/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Course(models.Model):
     title = models.CharField(max_length=200)
     instructor = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -9,11 +8,12 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+
 class Enrollment(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='enrollments')
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     grade = models.FloatField(null=True, blank=True)
     passed = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return f"{self.student} in {self.course}"
